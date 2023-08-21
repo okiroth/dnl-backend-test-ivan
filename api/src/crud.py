db3b36358b56c17dd5bed4310de1f27e88bca20f
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2023-08-21 19:18:35 -0300

    Add subquery when GET for Brand

--- api/src/crud.py
+++ api/src/crud.py
@@ -1,12 +1,17 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
-from . import models, schemas
+from . import models
 
 
 def get_brand(db: Session, name = ""):
     return (
         db.query(models.Brand)
         .filter(models.Brand.name == name)
+        .options(
+            joinedload(models.Brand.categories)
+            .subqueryload(models.Category.models)
+            .subqueryload(models.Model.parts)
+        )
         .first()
     )
 
