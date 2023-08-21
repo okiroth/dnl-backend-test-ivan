5396df08169500d021b551fd5cb63d2a5207ba18
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2023-08-21 17:59:42 -0300

    Add lazyloading to Brands

--- api/src/crud.py
+++ api/src/crud.py
@@ -1,10 +1,10 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, lazyload
 
 from . import models, schemas
 
 
 def get_brand(db: Session, brand_name):
-    return db.query(models.Brand).filter(models.Brand.name == brand_name).first()
+    return db.query(models.Brand).filter(models.Brand.name == brand_name).options(lazyload(models.Category.models)).first()
 
 
 def get_brands(db: Session, skip, limit):
--- api/src/main.py
+++ api/src/main.py
@@ -8,8 +8,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -19,15 +17,15 @@ def get_db():
 
 
 @app.get("/", response_model=list[schemas.Brand])
-def read_brands(skip: int = 0, limit: int = 1000, db: Session = Depends(get_db)):
-    db_res = crud.get_brands(db, skip=skip, limit=limit)
-    return db_res
+def read_brands(skip: int = 0, limit: int = 10, brand_name: str = None):
+    db: Session = Depends(get_db)
+    if brand_name is not None:
+        return crud.get_brand(db, brand_name)    
+    return crud.get_brands(db, skip, limit)
 
 
-@app.get("/?brand={brand_name}", response_model=schemas.Brand)
-def read_brand(brand_name: int, db: Session = Depends(get_db)):
-    db_res = crud.get_brand(db, brand_name=brand_name)
-    if db_res is None:
-        raise HTTPException(status_code=404, detail="Brand not found")
-    return db_res
+@app.get("/brand/{brand_name}", response_model=schemas.Brand)
+def read_brand(brand_name: str):
+    db: Session = Depends(get_db)
+    return crud.get_brand(db, brand_name)
 
