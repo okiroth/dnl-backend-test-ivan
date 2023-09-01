7c714910126bbb6c186cac5e481a36cffb467bde
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2023-09-01 17:15:39 -0300

    Extract nicer Code and Name in array form

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -2,26 +2,28 @@ from database_handler import save_json_to_DB
 import scrapy
 import logging
 
-logging.getLogger('scrapy').propagate = False
+# logging.getLogger('scrapy').propagate = False
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         "https://www.urparts.com/index.cfm/page/catalogue",
+        # "https://www.urparts.com/index.cfm/page/catalogue/Volvo/Loader%20Parts/L45"
     ]
 
 
     # took 384 seconds, 1 minute less than the old one
     def parse(self, response):
         org_url = response.url.replace('https://www.urparts.com/', '')
+        # models page has 2 divs with class c_container
         for sel in (response.css('div.c_container')[-1]).css('ul').css('li').css('a'):
             url = sel.xpath('@href').get()
             # check that we are not querying the parts urls
             # check that we are not querying a previous (shorter) url
             if (url is None) or ("page/catalogue?part=" in url) or (len(url) < len(org_url)):
                 yield {
+                    "content": sel.css('*::text').extract(),
                     "url": url,
-                    "brand": sel.css('::text').get(),
                 }
             else:
                 print(org_url, '---->', url)
