093a79e4a17495d78f9d49c606baa8cc4f84b5a3
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2023-09-01 16:51:38 -0300

    Nicer algorithm and catch edge cases returning home

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -1,5 +1,8 @@
 from database_handler import save_json_to_DB
 import scrapy
+import logging
+
+logging.getLogger('scrapy').propagate = False
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -10,15 +13,19 @@ class QuotesSpider(scrapy.Spider):
 
     # took 384 seconds, 1 minute less than the old one
     def parse(self, response):
+        org_url = response.url.replace('https://www.urparts.com/', '')
         for sel in (response.css('div.c_container')[-1]).css('ul').css('li').css('a'):
             url = sel.xpath('@href').get()
             # check that we are not querying the parts urls
-            if url is not None and "page/catalogue?part=" not in url:
+            # check that we are not querying a previous (shorter) url
+            if (url is None) or ("page/catalogue?part=" in url) or (len(url) < len(org_url)):
+                yield {
+                    "url": url,
+                    "brand": sel.css('::text').get(),
+                }
+            else:
+                print(org_url, '---->', url)
                 yield response.follow(url, self.parse)
-            yield {
-                "url": url,
-                "brand": sel.css('::text').get(),
-            }
 
     
     
