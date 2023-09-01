8e6b2da74f0b8d4f851bc1624a1d183560d86b67
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2023-09-01 15:53:43 -0300

    Fix duplicated .c_container in models

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -1,7 +1,4 @@
-import asyncio
-import json
 from database_handler import save_json_to_DB
-import time
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
@@ -13,7 +10,7 @@ class QuotesSpider(scrapy.Spider):
 
     # took 384 seconds, 1 minute less than the old one
     def parse(self, response):
-        for sel in response.css('div.c_container').css('ul').css('li').css('a'):
+        for sel in (response.css('div.c_container')[-1]).css('ul').css('li').css('a'):
             url = sel.xpath('@href').get()
             # check that we are not querying the parts urls
             if url is not None and "page/catalogue?part=" not in url:
